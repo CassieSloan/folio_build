b72ba9a133fa44b49661fbff06c92f11cfdb19bf
Cassie <cassandra.sloan16@gmail.com>
2020-05-01 15:44:11 +1000

    reverting

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,7 @@ import './index.scss';
 const Index = ({ location }) => (
   <Layout location={location}>
     <div className="wrapper">
-      <h1>Nuzzle Homepage</h1>
-      <h2>bellow is the embed link</h2>
-      <div>
-        <iframe src="https://app.acuityscheduling.com/schedule.php?owner=18392918&calendarID=3800259" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
-      </div>
+      <h1>Homepage</h1>
     </div>
   </Layout>
 );
